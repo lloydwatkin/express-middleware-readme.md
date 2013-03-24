add24c4f3363a08f8bd03f2d1126c278259cc1a3
Lloyd Watkin <lloyd@evilprofessor.co.uk>
2013-03-24 21:50:43 +0000

    Working code with HtmlWrap :)

--- README.md
+++ README.md
@@ -2,3 +2,39 @@ express-middleware-readme.md
 ============================
 
 Express middleware which parses your project root's README.md to HTML and exposes on a specified URL.
+
+
+== Usage ==
+
+=== Default Options ===
+
+```javaScript
+app.use(require('express-middleware-readme.md').run)
+```
+
+This will expose your README.md at the the path '/readme.md'.
+
+=== Set options ===
+
+```javaScript
+var readme = require('express-middleware-readme.md')
+readme.setOptions({
+    endpoint: [ '/readme.md', '/README.html' ],
+    htmlWrap: {
+        scripts: '/js/main.js',
+        styles: '/css/style.css',
+        meta: [
+            { charset: 'utf-8' }
+        ]
+    }
+})
+app.use(readme.run)
+```
+
+Notes: 
+
+* Each of 'endpoint', 'scripts', and 'styles' can be string values or arrays
+* An empty object as 'htmlWrap' results in doctype, html, head, and body tags added
+* 'meta' can be made up of 'content', 'charset', 'http-equiv', and 'name' attributes
+* 'filename' is the location of your README.md relative to project root (defaults to /README.md)
+* 'endpoint' defaults to '/readme.html' 
--- index.js
+++ index.js
@@ -1,30 +1,74 @@
 var content        = null
 var options        = null
 var defaultOptions = {
-    endpoint: '/readme.md',
+    endpoint: '/readme.html',
     filename: 'README.md',
+    htmlWrap: {}
 }
 
 exports.setOptions = setOptions = function(opts, callback) {
-	content            = null
-	options            = opts
+	content = ""
+	options = opts
+	
+	var addHead = function() {
+	    content += "<!DOCTYPE html>\n<html>\n<head>\n"
+	    if (options.htmlWrap.scripts) options.htmlWrap.scripts.forEach(function(script) {
+	        content += '<script type="text/javascript" src="' + script + '"></script>' + "\n"
+	    })
+	    if (options.htmlWrap.styles) options.htmlWrap.styles.forEach(function(style) {
+	        content += '<link href="' + style + '" rel="stylesheet" type="text/css" />' + "\n"
+	    })
+	    if (options.htmlWrap.meta) options.htmlWrap.meta.forEach(function(meta) {
+	    	content += '<meta ' 
+	    	if (meta.name) content += 'name="' + meta.name + '" '
+	    	if (meta.content) content += 'content="' + meta.content + '" '
+	    	if (meta['http-equiv']) content += 'http-equiv="' + meta['http-equiv'] + '" '
+	    	if (meta.charset) content += 'charset="' + meta.charset + '" '
+	    	content += ' />' + "\n"
+	    })
+	    content += "</head>\n<body>\n"
+	}
+	
+	var addFoot = function() {
+		if (!options.htmlWrap) return
+		content += "\n</body>\n</html>"
+	}
+
+    var checkValues = function() {
+    	if (!options.filename) options.filename = defaultOptions.filename
+    	var endpoints = options.endpoint || defaultOptions.endpoint
+        if (typeof endpoints == 'string') endpoints = [ endpoints ]
+        options.endpoint = []
+        endpoints.forEach(function(endpoint) {
+        	options.endpoint[endpoint] = true
+        })
+    	if (options.htmlWrap) {
+    		['scripts', 'styles', 'meta'].forEach(function(i) {
+    			if (options.htmlWrap[i]) {
+    		        if (options.htmlWrap[i] && typeof options.htmlWrap[i] == 'string')
+    			        options.htmlWrap[i] = [ options.htmlWrap[i] ]
+    			}
+    	    })
+        }
+    }
+    checkValues()
+    
 	var readmeLocation = require('path').resolve(process.cwd(), options.filename)
 	require('fs').readFile(readmeLocation, 'utf8', function(error, data) {
-		if (error) return console.error(error)
-		content = require('github-flavored-markdown').parse(data)
+		if (error) throw(error)
+		content = ""
+		addHead()
+		content += require('github-flavored-markdown').parse(data)
+		addFoot()
 		if (callback) callback()
 	})
 }
 
 exports.run = function(req, res, next) {
-	
 	var handleRequest = function() {
-	    if (true) 
-	        console.log("responding with content")
-	        return res.send(content)
+	    if (options.endpoint[req.path]) return res.send(content)
 	    next()
     }
-    console.log(options)
 	if (!options) return setOptions(defaultOptions, handleRequest)
 	handleRequest()
 }
